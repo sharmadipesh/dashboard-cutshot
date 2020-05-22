2c51e9e3c90a28f49ed6356637969936fb69de1c
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-05-22 14:16:15 +0530

    [BUILD] File Creation

---
+++ src/views/invoice/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Invoice extends Component {
+    render() {
+        return (
+            <div>
+                Invoice
+            </div>
+        );
+    }
+}
+
+export default Invoice;
---
+++ utils/SidebarContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SidebarContainer extends Component {
+    render() {
+        return (
+            <div>
+                SidebarContainer
+            </div>
+        );
+    }
+}
+
+export default SidebarContainer;
