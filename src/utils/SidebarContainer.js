fed6892a19818db2d1282c1e69c5ae94bab60c4a
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-05-24 05:29:24 +0530

    [FIX] Sidebar Issue

--- src/utils/SidebarContainer.js
+++ src/utils/SidebarContainer.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 class SidebarContainer extends Component {
+    componentDidMount = () => {
+        let sidebarParentNode = document.getElementById('sidebar-style-container').parentNode;
+        // sidebarParentNode.style.boxShadow='unset';
+        sidebarParentNode.style.boxShadow='1px 0 1px 1px rgba(221, 221, 221, 0.1)';
+    }
     render() {
         return (
-            <div>
+            <div id="sidebar-style-container">
                 SidebarContainer
             </div>
         );
