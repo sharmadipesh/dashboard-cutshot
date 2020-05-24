d8641ad767fe19251cdebaccef6bb34fd6338e7c
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-05-24 09:10:29 +0530

    [CHANGE] Header

--- src/utils/Header.js
+++ src/utils/Header.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import Icon from 'utils/Icon';
+import Routes from 'config/Routes';
 
 class Header extends Component {
+
+    getPageName = (pathname) => {
+        if(pathname === Routes.LandingPage){
+            return 'Dashboard'
+        }else if(pathname === Routes.Calender){
+            return 'Calender'
+        }else if(pathname === Routes.Inbox){
+            return 'Inbox'
+        }else if(pathname === Routes.Invoice){
+            return 'Invoicing'
+        }else if(pathname === Routes.Lab_Experiment){
+            return 'Lab / Experimental'
+        }else{
+            return 'Dashboard'
+        }
+    }
+
     render() {
+        const {pathname} = this.props.location;
         return (
             <div className="header-style-container d-flex">
                 <div className="part-one d-flex align-items-center justify-content-center">
@@ -10,15 +29,21 @@ class Header extends Component {
                 </div>
                 <div className="d-flex align-items-center justify-content-between part-two">
                     <div className="title-text">
-                        Dashboard
+                        {/* Dashboard */}
+                        {this.getPageName(pathname)}
                     </div>
                     <div className="select-width">
-                        <select
+                        {/* <select
                             name="Language"
-                            className="form-control select-option"
+                            className="form-control"
                         >
                             <option value="English">English</option>
-                        </select>
+                        </select> */}
+                        <div className="">
+                            <select className="form-control font-size-14" name="Language">
+                                <option value="English">English</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div> 
--- src/utils/NotFound.js
+++ src/utils/NotFound.js
@@ -4,7 +4,7 @@ import Icon from 'utils/Icon';
 class NotFound extends Component {
     render() {
         return (
-            <div className="mt-80 d-flex flex-column align-items-center justify-content-center">
+            <div className="not-found-container d-flex flex-column align-items-center justify-content-center">
                 <Icon className="not-found-icon" icon="ICON_NOT_FOUND"/>
             </div>
         );
